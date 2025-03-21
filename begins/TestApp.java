670c62f21f270c18b99b8695798636ba2300dba6
Aliya <aliyahassan032@gmail.com>
2025-03-21 13:40:42 +0530

    begins

---
+++ begins/TestApp.java
@@ -0,0 +1,8 @@
+public class TestApp {
+	public static void main(String[] args) {
+		int a = 5, b= 7;	
+		System.out.println(a+b);
+		System.out.println("Sum is: " + a+b);
+		System.out.println("Sum is: "+ (a+b));
+	}
+}
