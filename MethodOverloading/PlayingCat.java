3f735c72d64cddd9d07ed55d01114c2c23739d40
Aliya <aliyahassan032@gmail.com>
2025-03-21 13:39:02 +0530

    untracked files

--- MethodOverloading/PlayingCat.java
+++
@@ -1,7 +0,0 @@
-public class PlayingCat {
-    public static boolean isCatPlaying(boolean summer, int temperature) {
-
-        int max = summer ? 45 : 35;
-        return temperature >= 25 && temperature <= max;
-    }
-}
