6c8e48ac22d1f3e152ca3af6c0488883b555c75c
Aliya <aliyahassan032@gmail.com>
2025-03-21 13:46:16 +0530

    deleted

--- TestApp.java
+++
@@ -1,8 +0,0 @@
-public class TestApp {
-	public static void main(String[] args) {
-		int a = 5, b= 7;	
-		System.out.println(a+b);
-		System.out.println("Sum is: " + a+b);
-		System.out.println("Sum is: "+ (a+b));
-	}
-}
